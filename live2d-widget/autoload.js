82356c9ff202dfdac9fd9d3887c1cad3f2e51f5f
zhaopan <1406971292@qq.com>
2020-01-03 16:14:19 +0800

    Site updated: 2020-01-03 16:14:19

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 03 2020 15:58:51 GMT+0800 (中国标准时间)
+// build time:Fri Jan 03 2020 16:14:09 GMT+0800 (中国标准时间)
 const live2d_path="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";$("<link>").attr({href:live2d_path+"waifu.css",rel:"stylesheet"}).appendTo("head");$.ajax({url:live2d_path+"live2d.min.js",dataType:"script",cache:true});$.ajax({url:live2d_path+"waifu-tips.js",dataType:"script",cache:true});$(window).on("load",function(){initWidget(live2d_path+"waifu-tips.json","https://live2d.fghrsh.net/api")});
 //rebuild by neat 
