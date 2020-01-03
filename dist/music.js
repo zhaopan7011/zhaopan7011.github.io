49bd1fbcb57a8e779bb087e15b4541473b702512
zhaopan <1406971292@qq.com>
2020-01-03 10:37:41 +0800

    Site updated: 2020-01-03 10:37:41

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 03 2020 10:29:23 GMT+0800 (中国标准时间)
+// build time:Fri Jan 03 2020 10:37:31 GMT+0800 (中国标准时间)
 const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,lrcType:0,audio:[{name:"无尽光芒",artist:"许巍",url:"http://www.ytmp3.cn/down/57563.mp3",cover:"https://s2.ax1x.com/2019/08/08/e7uwzn.png",lrc:"LRC/许巍-无尽光芒.lrc"},{name:"曾经的你",artist:"许巍",url:"http://www.ytmp3.cn/down/43808.mp3",cover:"https://s2.ax1x.com/2019/08/08/e7M5VK.png"},{name:"我的爱",artist:"许巍",url:"http://www.ytmp3.cn/down/59641.mp3",cover:"https://s2.ax1x.com/2019/08/08/e7M5VK.png"}]});
 //rebuild by neat 
