59d15f2f8757ddb7cec3e1912489b6152f13365f
zhaopan <1406971292@qq.com>
2020-01-08 16:07:46 +0800

    Site updated: 2020-01-08 16:07:46

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Wed Jan 08 2020 15:41:44 GMT+0800 (中国标准时间)
+// build time:Wed Jan 08 2020 16:05:48 GMT+0800 (中国标准时间)
 window.onload=function(){var e=document.title;var t;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","../../images/failure.png");$('[rel="shortcut icon"]').attr("href","../../images/failure.png");document.title="(●—●)喔哟，崩溃啦！";clearTimeout(t)}else{$('[rel="icon"]').attr("href","../../images/favicon-32x32-next.png");$('[rel="shortcut icon"]').attr("href","../../images/favicon-32x32.png");document.title="(/≧▽≦/)咦！页面又好了！";t=setTimeout(function(){document.title=e},2e3)}})};
 //rebuild by neat 
